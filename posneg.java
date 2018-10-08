d4c248e1f031be33abe603a966d7a726c68e6eb3
sikkandarbatcha <sikkandarbatcha15@gmail.com>
2018-10-08 05:43:01 -0700

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int n= sc.nextInt();
+if(n>0)
+{
+  System.out.println("POSITIVE");
+}
+else if(n<0)
+{
+ System.out.println("NEGATIVE");
+}
+else {
+ System.out.println("ZERO");
+ }
+} 
